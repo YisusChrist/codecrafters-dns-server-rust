37ff80825e1bf285e147beb1f1994ed9d97a08f3
Alejandro González Momblán <agelrenorenardo@gmail.com>
2023-11-23 15:11:57 +0100

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,67 @@
 // Uncomment this block to pass the first stage
 use std::net::UdpSocket;
 
+// Connect to DNS server on http://127.0.0.1:2053
+
+const SERVER_PORT: &str = "2053";
+const SERVER_ADDRESS: &str = "127.0.0.1";
+const PACKET_IDENTIFIER: u16 = 1234;
+
+struct DnsHeader {
+    id: u16,
+    qr: u8,
+    opcode: u8,
+    aa: u8,
+    tc: u8,
+    rd: u8,
+    ra: u8,
+    z: u8,
+    rcode: u8,
+    qdcount: u16,
+    ancount: u16,
+    nscount: u16,
+    arcount: u16,
+}
+
+impl DnsHeader {
+    fn new() -> DnsHeader {
+        DnsHeader {
+            id: PACKET_IDENTIFIER,
+            qr: 1,
+            opcode: 0,
+            aa: 0,
+            tc: 0,
+            rd: 0,
+            ra: 0,
+            z: 0,
+            rcode: 0,
+            qdcount: 0,
+            ancount: 0,
+            nscount: 0,
+            arcount: 0,
+        }
+    }
+
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = Vec::with_capacity(12);
+        bytes.extend_from_slice(&self.id.to_be_bytes());
+        bytes.push((self.qr << 7) | (self.opcode << 3) | (self.aa << 2) | (self.tc << 1) | self.rd);
+        bytes.push((self.ra << 7) | (self.z << 4) | self.rcode);
+        bytes.extend_from_slice(&self.qdcount.to_be_bytes());
+        bytes.extend_from_slice(&self.ancount.to_be_bytes());
+        bytes.extend_from_slice(&self.nscount.to_be_bytes());
+        bytes.extend_from_slice(&self.arcount.to_be_bytes());
+        bytes
+    }
+}
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
+    let udp_socket = UdpSocket::bind(format!("{}:{}", SERVER_ADDRESS, SERVER_PORT))
+        .expect("Failed to bind to address");
     let mut buf = [0; 512];
 
     loop {
@@ -14,7 +69,10 @@ fn main() {
             Ok((size, source)) => {
                 let _received_data = String::from_utf8_lossy(&buf[0..size]);
                 println!("Received {} bytes from {}", size, source);
-                let response = [];
+
+                let dns_header = DnsHeader::new();
+                let response = dns_header.to_bytes();
+
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
